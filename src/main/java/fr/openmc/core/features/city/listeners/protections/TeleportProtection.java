992ee13726591db78ffd990b7f3498e712a8828c
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-07-12 11:39:12 +0200

    fix: teleportation in city

--- src/main/java/fr/openmc/core/features/city/listeners/protections/TeleportProtection.java
+++ src/main/java/fr/openmc/core/features/city/listeners/protections/TeleportProtection.java
@@ -9,10 +9,21 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Set;
+
 public class TeleportProtection implements Listener {
+    private final Set<PlayerTeleportEvent.TeleportCause> illegalCauses = Set.of(
+            PlayerTeleportEvent.TeleportCause.COMMAND,
+            PlayerTeleportEvent.TeleportCause.NETHER_PORTAL,
+            PlayerTeleportEvent.TeleportCause.PLUGIN,
+            PlayerTeleportEvent.TeleportCause.SPECTATE
+    );
     @EventHandler
     public void onPlayerTeleport(PlayerTeleportEvent event) {
         if (event.isCancelled()) return;
+        PlayerTeleportEvent.TeleportCause cause = event.getCause();
+
+        if (illegalCauses.contains(cause)) return;
 
         Player player = event.getPlayer();
 
