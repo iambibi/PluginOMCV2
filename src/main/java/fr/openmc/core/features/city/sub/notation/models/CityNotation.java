e6566ae8c7802826882c4f137c21e9a966b70a86
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-07-30 18:04:34 +0200

    fix: table name

--- src/main/java/fr/openmc/core/features/city/sub/notation/models/CityNotation.java
+++ src/main/java/fr/openmc/core/features/city/sub/notation/models/CityNotation.java
@@ -1,9 +1,11 @@
 package fr.openmc.core.features.city.sub.notation.models;
 
 import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
 import fr.openmc.core.utils.DateUtils;
 import lombok.Getter;
 
+@DatabaseTable(tableName = "city_notation")
 @Getter
 public class CityNotation {
     @DatabaseField(id = true, columnName = "uuid")
