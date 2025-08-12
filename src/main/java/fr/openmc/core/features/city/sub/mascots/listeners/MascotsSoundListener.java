8851512f8e86d90e1d669e8f4ff50aa81e38489b
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-08-12 16:57:44 +0200

    add: mute settings

--- src/main/java/fr/openmc/core/features/city/sub/mascots/listeners/MascotsSoundListener.java
+++ src/main/java/fr/openmc/core/features/city/sub/mascots/listeners/MascotsSoundListener.java
@@ -4,37 +4,67 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.reflect.accessors.FieldAccessor;
 import fr.openmc.core.OMCPlugin;
 import fr.openmc.core.features.city.sub.mascots.MascotsManager;
+import fr.openmc.core.features.city.sub.mascots.models.Mascot;
 import fr.openmc.core.features.settings.PlayerSettingsManager;
 import fr.openmc.core.features.settings.SettingType;
-import net.minecraft.server.level.ServerLevel;
-import org.bukkit.craftbukkit.CraftWorld;
-import org.bukkit.entity.Player;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+
+import java.util.List;
+import java.util.UUID;
 
 public class MascotsSoundListener {
 
     public MascotsSoundListener() {
-        ProtocolLibrary.getProtocolManager().addPacketListener(
-                new PacketAdapter(OMCPlugin.getInstance(), ListenerPriority.NORMAL, PacketType.Play.Server.ENTITY_SOUND) {
-                    @Override
-                    public void onPacketSending(PacketEvent event) {
-                        Player player = event.getPlayer();
-                        if (PlayerSettingsManager.getPlayerSettings(player.getUniqueId()).getSetting(SettingType.MASCOT_PLAY_SOUND_POLICY)) {
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(
+                OMCPlugin.getInstance(),
+                ListenerPriority.NORMAL,
+                PacketType.Play.Server.NAMED_SOUND_EFFECT
+        ) {
+            @Override
+            public void onPacketSending(PacketEvent event) {
+                PacketContainer packet = event.getPacket();
 
-                            int entityId = event.getPacket().getIntegers().read(0);
-
-                            ServerLevel nmsLevel = ((CraftWorld) player.getWorld()).getHandle();
-
-                            net.minecraft.world.entity.Entity nmsEntity = nmsLevel.getEntity(entityId);
-
-                            if (nmsEntity != null && MascotsManager.mascotsByEntityUUID.containsKey(nmsEntity.getUUID())) {
-                                event.setCancelled(true);
-                            }
-                        }
-                    }
+                for (FieldAccessor fuield : packet.getStructures().getFields()) {
+                    System.out.println(fuield.getField().getType().getName() + " " + fuield.getField().getName());
                 }
-        );
+
+                String soundName = packet.getSoundEffects().read(0).toString();
+                String[] splitedSound = soundName.split("\\.");
+
+                if (!splitedSound[0].equals("ENTITY")) return;
+
+                UUID playerUUID = event.getPlayer().getUniqueId();
+                if (PlayerSettingsManager.getPlayerSettings(playerUUID).getSetting(SettingType.MASCOT_PLAY_SOUND_POLICY))
+                    return;
+
+                double x = packet.getIntegers().read(0) / 8.0;
+                double y = packet.getIntegers().read(1) / 8.0;
+                double z = packet.getIntegers().read(2) / 8.0;
+
+                World world = event.getPlayer().getWorld();
+
+                Location location = new Location(world, x, y, z);
+
+                List<Mascot> mascotsNear = world.getNearbyEntities(location, 16, 16, 16)
+                        .stream()
+                        .filter(entity -> MascotsManager.mascotsByEntityUUID.containsKey(entity.getUniqueId()))
+                        .map(entity -> MascotsManager.mascotsByEntityUUID.get(entity.getUniqueId()))
+                        .toList();
+
+                mascotsNear.forEach(mascot -> {
+                    if (mascot.getEntity().getType().equals(EntityType.fromName(splitedSound[1].toUpperCase()))) {
+                        event.setCancelled(true);
+                        return;
+                    }
+                });
+            }
+        });
     }
 }
