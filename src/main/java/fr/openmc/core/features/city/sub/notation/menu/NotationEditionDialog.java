780a2e0b419710b62626831c046e86479e876eeb
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-08-18 16:50:31 +0200

    fix review

--- src/main/java/fr/openmc/core/features/city/sub/notation/menu/NotationEditionDialog.java
+++ src/main/java/fr/openmc/core/features/city/sub/notation/menu/NotationEditionDialog.java
@@ -13,6 +13,7 @@ import io.papermc.paper.registry.data.dialog.ActionButton;
 import io.papermc.paper.registry.data.dialog.DialogBase;
 import io.papermc.paper.registry.data.dialog.action.DialogAction;
 import io.papermc.paper.registry.data.dialog.body.DialogBody;
+import io.papermc.paper.registry.data.dialog.input.DialogInput;
 import io.papermc.paper.registry.data.dialog.input.TextDialogInput;
 import io.papermc.paper.registry.data.dialog.type.DialogType;
 import net.kyori.adventure.text.Component;
