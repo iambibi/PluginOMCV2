d6c82c87c1e193c44e75a7cb79e6b56c03c48d71
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-10-29 13:22:54 +0100

    add better cooldown

--- src/main/java/fr/openmc/core/features/dream/enchantements/DreamSleeper.java
+++ src/main/java/fr/openmc/core/features/dream/enchantements/DreamSleeper.java
@@ -1,6 +1,5 @@
 package fr.openmc.core.features.dream.enchantements;
 
-import fr.openmc.api.cooldown.DynamicCooldownManager;
 import fr.openmc.core.OMCPlugin;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
@@ -17,8 +16,6 @@ public class DreamSleeper implements Listener {
     public void onAttack(EntityDamageByEntityEvent event) {
         if (!(event.getDamager() instanceof Player player)) return;
 
-        if (!DynamicCooldownManager.isReady(player.getUniqueId(), "dream:dream_sleeper")) return;
-
         Entity entityAttacked = event.getEntity();
         if (entityAttacked instanceof Player) return;
 
@@ -28,8 +25,10 @@ public class DreamSleeper implements Listener {
             Enchantment enchantment = DreamEnchantementRegistry.getDreamEnchantment("dream:dream_sleeper");
             if (!item.getEnchantments().containsKey(enchantment)) return;
 
+            if (player.hasCooldown(item)) return;
+
+            player.setCooldown(item, 5 * 20);
             livingEntity.setAI(false);
-            DynamicCooldownManager.use(player.getUniqueId(), "dream:dream_sleeper", 5 * 20L);
 
             int level = item.getEnchantments().get(enchantment);
 
