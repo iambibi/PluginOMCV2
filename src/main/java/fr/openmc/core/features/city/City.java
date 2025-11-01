16abb66586b6b8113067fd436c80dfbae3e91e8a
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-11-01 18:34:56 +0100

    Correctif du coffre de ville (drag, reset when kick) (#1053)
    * fix city chest drag
    
    * fix city chest reset when leave/quit by admin
    
    * Remove {}
    
    ---------
    
    Co-authored-by: PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>

--- src/main/java/fr/openmc/core/features/city/City.java
+++ src/main/java/fr/openmc/core/features/city/City.java
@@ -207,19 +207,24 @@ public class City {
      * Allows a player to leave a city and updates the database and region
      * permissions.
      *
-     * @param player The UUID of the player leaving the city.
+     * @param playerUUID The UUID of the player leaving the city.
      */
-    public void removePlayer(UUID player) {
+    public void removePlayer(UUID playerUUID) {
         if (this.members == null)
             this.members = CityManager.getCityMembers(this);
 
-        members.remove(player);
-        permissions.remove(player);
+        OfflinePlayer offlinePlayer = CacheOfflinePlayer.getOfflinePlayer(playerUUID);
+
+        if (offlinePlayer.isOnline() && offlinePlayer instanceof Player player)
+            player.closeInventory();
+
+        members.remove(playerUUID);
+        permissions.remove(playerUUID);
         Bukkit.getScheduler().runTask(OMCPlugin.getInstance(), () ->
             Bukkit.getPluginManager()
-                    .callEvent(new MemberLeaveEvent(CacheOfflinePlayer.getOfflinePlayer(player), this))
+                    .callEvent(new MemberLeaveEvent(CacheOfflinePlayer.getOfflinePlayer(playerUUID), this))
         );
-        CityManager.removePlayerFromCity(this, player);
+        CityManager.removePlayerFromCity(this, playerUUID);
     }
 
     /**
