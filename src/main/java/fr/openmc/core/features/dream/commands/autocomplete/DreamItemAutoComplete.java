4ac0bec29cddbff872924a7b8bb0829306a932a4
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-11-16 18:03:04 +0100

    review

--- src/main/java/fr/openmc/core/features/dream/commands/autocomplete/DreamItemAutoComplete.java
+++ src/main/java/fr/openmc/core/features/dream/commands/autocomplete/DreamItemAutoComplete.java
@@ -6,15 +6,26 @@ import revxrsal.commands.autocomplete.SuggestionProvider;
 import revxrsal.commands.bukkit.actor.BukkitCommandActor;
 import revxrsal.commands.node.ExecutionContext;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DreamItemAutoComplete implements SuggestionProvider<BukkitCommandActor> {
 
     @Override
     public @NotNull List<String> getSuggestions(@NotNull ExecutionContext<BukkitCommandActor> context) {
-        return DreamItemRegistry.getNames()
-                .stream()
-                .map(name -> name.replace("omc_dream:", ""))
-                .toList();
+        List<String> result = new ArrayList<>();
+
+        for (String name : DreamItemRegistry.getNames()) {
+            String prefix = "omc_dream:";
+            int prefixLength = prefix.length();
+
+            if (name.length() > prefixLength && name.startsWith(prefix)) {
+                result.add(name.substring(prefixLength));
+            } else {
+                result.add(name);
+            }
+        }
+
+        return result;
     }
 }
